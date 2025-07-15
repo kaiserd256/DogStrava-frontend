6ddb9c444e4966e65acf7e387b03925f514bdfcd
blincoechad <blincoechad@gmail.com>
2025-07-15 18:46:53 -0400

    chads first commit

--- dog-strava/src/app/page.tsx
+++ dog-strava/src/app/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <p>Hello Dog Strava</p>
+    <div>
+      <p>Hello Dog Strava</p>
+      <p>Acitivty</p>
+    </div>
   );
 }
